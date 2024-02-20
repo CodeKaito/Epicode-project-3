9873bc2c3a2eea2bec96135c2ea14c70c4b177e9
Jerome Decinco <57111980+CodeKaito@users.noreply.github.com>
2024-02-20 12:18:36 +0100

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const apiUrl = "https://striveschool-api.herokuapp.com/api/product";
-
-const authorizationToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWNkZGU5MGUwZWVkODAwMWEzY2FkNjEiLCJpYXQiOjE3MDgzNjcyOTAsImV4cCI6MTcwOTU3Njg5MH0.hxAVd25Y1IiaQFxzOn8nHtkhpHiuOuzNwBSZT_pyrt4"
-
-export { authorizationToken };
-export default apiUrl;
