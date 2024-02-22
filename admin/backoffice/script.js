877f90a547c5d381981b840e29514e1716694782
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2024-02-22 20:03:32 +0100

    update

--- admin/backoffice/script.js
+++ admin/backoffice/script.js
@@ -5,40 +5,69 @@ import { authorizationToken } from "/index.js";
 
 // Global variables
 let productTitle = document.getElementById('productTitle');
-
 let productDesc = document.getElementById('productDesc');
-
 let productBrand = document.getElementById('productBrand');
-
 let imageUrl = document.getElementById('imageUrl');
-
 let productPrice = document.getElementById('productPrice');
-
 let cardCreation = document.getElementById('cardCreation');
 
+// Button per aggiungere il prodotto
 const addBtn = document.getElementById('addBtn');
 
+// Aggiunge un event listener per il blur sull'elemento productTitle
+productTitle.addEventListener('blur', async function (e) {
+    // Ottiene il valore dell'input al momento del blur
+    const valoreInput = e.target.value;
+    // Inizio del try catch
+    try {
+        // Effettua una chiamata API di tipo GET per ottenere i dati
+        let response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${authorizationToken}`,
+                'Content-Type': 'application/json',
+            },
+        });
+
+        // Estrae i dati dalla risposta
+        let data = await response.json();
+
+        // Estrae i titoli dalla risposta API
+        const titoliAPI = data.map(item => item.name);
+
+        // Controlla se il valore dell'input è già presente nei titoli ottenuti dall'API
+        if (titoliAPI.includes(valoreInput)) {
+            // Se sì, mostra un avviso all'utente
+            alert('Attenzione! Esiste già un prodotto con lo stesso titolo');
+        }
+    } catch (error) {
+        // Gestisce gli errori durante la chiamata API
+        console.error("Errore durante la chiamata GET API:", error);
+    }
+});
+
+
+// Degli addEventListener che captano ció che ho in input per aggiornare la card che funge da anteprima
 productTitle.addEventListener('input', updateCard);
-
 productDesc.addEventListener('input', updateCard);
-
 productBrand.addEventListener('input', updateCard);
-
 imageUrl.addEventListener('input', updateCard);
-
 productPrice.addEventListener('input', updateCard);
 
+// Controllo del prezzo per evitare che qualcuno inserisca delle lettere
 productPrice.addEventListener('input', function (e) {
-    
     let inputValue = e.target.value;
     inputValue = inputValue.replace(/[^0-9]/g, '');
     e.target.value = inputValue;
 });
 
-// La funzione che permette alla card di anteprima del product di esister
+// La funzione che permette alla card di anteprima del product di esistere
 function updateCard() {
+    // Il clear ogni volta che l'updateCard viene chiamato perché se no mi duplica n volte la card quante sono le chiamate!
     clearCardCreation();
+    // Valorizzazione della card di anteprima
     cardExample(productTitle.value, productDesc.value, imageUrl.value, productBrand.value, productPrice.value);
+    // Mi controlla se tutti gli input sono popolati
     checkAllInput();
 }
 
@@ -52,9 +81,8 @@ async function fetchData() {
         imageUrl: imageUrl.value,
         price: productPrice.value,
     };
-
-    checkAllInput();
-
+    
+    // Chiamata api di tipo POST con body postData
     try {
         let response = await fetch(apiUrl, {
             method: 'POST',
@@ -66,10 +94,11 @@ async function fetchData() {
         });
 
         let data = await response.json();
-        console.log(data);
 
+        // Ogni volta che aggiungo una card, ho un alert che mi dice che la chiamata POST é andata a buon fine e il nome del prodotto
         alert('Product created successfully: ' + data.name)
-
+        
+        // Dopo aver aggiunto il prodotto al api mi sposto sulla pagina principale /admin/homepage
         window.location.href = '/admin/homepage/index.html';
 
     } catch (error) {
@@ -77,8 +106,10 @@ async function fetchData() {
     }
 }
 
+// Il button ha evento click per richiamare la funzione per fare la chiamata API di tipo POST
 addBtn.addEventListener('click', fetchData);
 
+// Funzione per creare la logica per evitare che si creino infinite cards di anteprima
 function clearCardCreation() {
     // Rimuovo tutte le card create per evitare che si creino infinite cards
     while (cardCreation.firstChild) {
@@ -86,7 +117,7 @@ function clearCardCreation() {
     }
 }
 
-// FUnzione per controllare se tutti gli input sono correttamente popolati, altrimenti inserisce un disabled al button addBtn se non esiste giá nel dom
+// Funzione per controllare se tutti gli input sono correttamente popolati, altrimenti inserisce un disabled al button addBtn se non esiste giá nel dom
 function checkAllInput() {
     const title = productTitle.value.trim();
     const desc = productDesc.value.trim();
@@ -101,7 +132,8 @@ function checkAllInput() {
     }
 }
 
-async function cardExample(title, description, image, brand, price) {
+// Creazione della funzione per creare la card di anteprima
+let cardExample = (title, description, image, brand, price) => {
     let divCard = document.createElement('div');
         divCard.classList.add('card', 'text-center');
 
