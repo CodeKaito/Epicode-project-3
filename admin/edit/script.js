ecdf34dac5304dc4c685d12a388506a99a433823
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-02-23 10:12:41 +0100

    aggiunta di alert nella sezione edit

--- admin/edit/script.js
+++ admin/edit/script.js
@@ -8,24 +8,52 @@ import { failureAlert } from '/utils/alert.js';
 
 // Global variables
 let productTitle = document.getElementById('productTitle');
-
 let productDesc = document.getElementById('productDesc');
-
 let productBrand = document.getElementById('productBrand');
-
 let imageUrl = document.getElementById('imageUrl');
-
 let productPrice = document.getElementById('productPrice');
 
+// Aggiunge un event listener per il blur sull'elemento productTitle
+productTitle.addEventListener('blur', async function (e) {
+    // Ottiene il valore dell'input al momento del blur
+    const valoreInput = e.target.value;
+    // Inizio del try catch
+    try {
+        // Effettua una chiamata API di tipo GET per ottenere i dati
+        let response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${authorizationToken}`,
+                'Content-Type': 'application/json',
+            },
+        });
+
+        // Estrae i dati dalla risposta
+        let data = await response.json();
+
+        // Estrae i titoli dalla risposta API
+        const titoliAPI = data.map(item => item.name);
+
+        // Controlla se il valore dell'input è già presente nei titoli ottenuti dall'API
+        if (titoliAPI.includes(valoreInput)) {
+            // Se sì, mostra un avviso all'utente
+            // alert('Attenzione! Esiste già un prodotto con lo stesso titolo');
+            failureAlert("Error! There's already a product with this name");
+        }
+    } catch (error) {
+        // Gestisce gli errori durante la chiamata API
+        console.error("Errore durante la chiamata GET API:", error);
+    }
+});
+
+// Degli addEventListener che captano ció che ho in input per aggiornare la card che funge da anteprima
 productTitle.addEventListener('input', updateCard);
-
 productDesc.addEventListener('input', updateCard);
-
 productBrand.addEventListener('input', updateCard);
-
 imageUrl.addEventListener('input', updateCard);
-
 productPrice.addEventListener('input', updateCard);
+
+
 
 function updateCard() {
     clearCardCreation();
