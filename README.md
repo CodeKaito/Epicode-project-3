7c3a24090fe3600dcae0e1846cf3a3bbf299ce2c
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-02-23 14:18:09 +0100

    update

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Repository del terzo Progetto di Epicode
 
 ## Endpoint
 https://striveschool-api.herokuapp.com/api/product/
+
--- utils/alert.js
+++ utils/alert.js
@@ -1,3 +1,4 @@
+// Creazione della funzione che crea l'alert
 const createAlert = (message, colorClass, timeout=null) => {
     const alert = document.createElement('div');
     const alertButton = document.createElement('button');
@@ -17,6 +18,7 @@ const createAlert = (message, colorClass, timeout=null) => {
     document.body.appendChild(alert);
 }
 
+// Suddivisione dei due alert che avranno come background color verde se success, rosso se failed
 const successAlert = (message, timeout=null) => {
     createAlert(message, 'alert-success', timeout);
 }
@@ -25,5 +27,6 @@ const failureAlert = (message, timeout=null) => {
     createAlert(message, 'alert-failed', timeout);
 }
 
+// Esportazione delle due funzioni per il riuso in altri file js
 export default successAlert;
 export { failureAlert };
